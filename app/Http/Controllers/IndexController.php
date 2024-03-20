c83b884341c9572210f029a99f65c3b021b3f3a0
Geferson Moreno <leonaikervan2001@gmail.com>
2024-03-20 17:56:18 -0400

    creando los seeder y factory y controller index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActaEntrega;
+use App\Models\ActuacionFiscal;
+use App\Models\PersonalEntrega;
+use App\Models\PersonalRecibe;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $data = [
+            'users' => User::all(),
+            'activities' => ActuacionFiscal::all(),
+            'handoverRecords' => ActaEntrega::all(),
+            'outgoing' => PersonalRecibe::all(),
+            'incoming' => PersonalEntrega::all(),
+        ];
+
+        return view('index', $data);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,6 @@
-@extends('layouts.template')
 
-@section('title', 'UAI')
-
-@section('content')
-<h1>soy index</h1>
-{{-- <form action="{{ route('cargo.guardar') }}" method="post">
-@csrf
-<input style="margin-top: 20vh" type="text" name="nombre">
-<input type="submit" value="enviar">
-<input type="hidden" name="_token" value="{{ csrf_token() }}">
-</form> --}}
-  
-@endsection
+[
+  {{$users}},
+  {{$activities}},
+  {{$handoverRecords}}
+]
